ecf27ee85ce22c855e7276388a95fa13ba6c204b
Mark Davies <mdavies2@webroot.com>
2019-11-20 13:28:11 +0000

    First couple of classes

---
+++ src/Leaflex.Web/Config/LocalStackConfig.cs
@@ -0,0 +1,30 @@
+﻿namespace Leaflex.Web.Config
+{
+    public class LocalStackConfig
+    {
+        public string ApiGateway { get; set; } = "4567";
+        public string Kinesis { get; set; } = "4568";
+        public string DynamoDb { get; set; } = "4569";
+        public string DynamoStreams { get; set; } = "4570";
+        public string ElasticSearch { get; set; } = "4571";
+        public string S3 { get; set; } = "4572";
+        public string FireHose { get; set; } = "4573";
+        public string Lamda { get; set; } = "4574";
+        public string Sns { get; set; } = "4575";
+        public string Sqs { get; set; } = "4576";
+        public string RedShift { get; set; } = "4577";
+        public string ElasticSearchServices { get; set; } = "4578";
+        public string Ses { get; set; } = "4579";
+        public string Route53 { get; set; } = "4580";
+        public string CloudFormation { get; set; } = "4581";
+        public string CloudWatch { get; set; } = "4582";
+        public string Ssm { get; set; } = "4583";
+        public string SecretsManager { get; set; } = "4584";
+        public string StepsFunctions { get; set; } = "4585";
+        public string CloudWatchLogs { get; set; } = "4586";
+        public string EventBridge { get; set; } = "4587";
+        public string Sts { get; set; } = "4592";
+        public string Iam { get; set; } = "4593";
+        public string Ecs { get; set; } = "4597";
+    }
+}
--- src/Leaflex.Web/Startup.cs
+++ src/Leaflex.Web/Startup.cs
@@ -1,37 +1,32 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Leaflex.Web.Data;
+using Leaflex.Web.Config;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Leaflex.Web.Data;
 
 namespace Leaflex.Web
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<LocalStackConfig>("Ports", _configuration);
+
             services.AddRazorPages();
             services.AddServerSideBlazor();
+
             services.AddSingleton<WeatherForecastService>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -41,7 +36,6 @@ namespace Leaflex.Web
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
