4940db07707e708d027aa927bca8d4a8cda3b5f9
Mark Davies <mdavies2@webroot.com>
2019-12-09 09:50:55 +0000

    Copying prototype testing framewokr for blazor

---
+++ src/Leaflex.Web.Tests/S3/ListTests.cs
@@ -0,0 +1,28 @@
+﻿using Leaflex.Web.Pages;
+using Leaflex.Web.S3;
+using Microsoft.AspNetCore.Components.Testing;
+using Xunit;
+
+namespace Leaflex.Web.Tests.S3
+{
+    public class ListTests
+    {
+        private readonly TestHost _host = new TestHost();
+
+        [Fact]
+        public void Thing()
+        {
+            var component = _host.AddComponent<Counter>();
+            Assert.Equal("Current count: 0", component.Find("p").InnerText);
+        }
+
+        [Fact]
+        public void CanIncrementCount()
+        {
+            var component = _host.AddComponent<Counter>();
+
+            component.Find("button").Click();
+            Assert.Equal("Current count: 1", component.Find("p").InnerText);
+        }
+    }
+}
